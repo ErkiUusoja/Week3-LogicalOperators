eb6eccee88a6a93b5268c984381ed1107a8a696e
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:07:12 +0300

    master

---
+++ Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sisestada kasutajatunnus;
+            //programm küsib kasutajal sisestada parool;
+            //programm kontrollib, kas mõlemad nii kasutajatunnus
+            //kui ka parool on õiged
+            //kui mõlemad, kasuajatunnus ja parool on õiged
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis
+            //programm kuvab "sisselogimine ebaõnnestus. Proovi uuesti."
+            //kt: admin, pr:admin1234
+
+            Console.WriteLine("Sisesta kasutajatunnus");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool");
+            string userPassWord = Console.ReadLine();
+
+            if (userName != "admin" || userPassWord != "admin1234")
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus, proovi uuesti!");
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+
+
+
+        }
+    }
+}
